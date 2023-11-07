e34d646152c1a6cb4e4c58420d3899a5c1a5561c
tarpetos <hevlichtaras@gmail.com>
2023-11-07 13:08:06 +0200

    Migrate to aiogram init

---
+++ bot/handlers/echo.py
@@ -0,0 +1,23 @@
+from aiogram import types, Router, F
+from aiogram.filters import StateFilter
+from aiogram.fsm.context import FSMContext
+from aiogram.utils.markdown import hcode
+
+echo_router = Router()
+
+
+@echo_router.message(F.text, StateFilter(None))
+async def bot_echo(message: types.Message):
+    text = ["Echo without state.", "Message:", message.text]
+    await message.answer("\n".join(text))
+
+
+@echo_router.message(F.text)
+async def bot_echo_all(message: types.Message, state: FSMContext):
+    state_name = await state.get_state()
+    text = [
+        f"Echo with state {hcode(state_name)}",
+        "Message:",
+        hcode(message.text),
+    ]
+    await message.answer("\n".join(text))
